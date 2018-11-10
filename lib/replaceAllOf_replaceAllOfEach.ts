20d0b90a26b3c5f95aba504a0e82055395d6c36c
Steve Thompson <writetome51@gmail.com>
2018-11-09 22:16:54 -0700

    minor refactoring

--- lib/replaceAllOf_replaceAllOfEach.ts
+++ lib/replaceAllOf_replaceAllOfEach.ts
@@ -1,5 +1,6 @@
-import { arrayHas } from '@writetome51/arrayHas';
+import { arrayHas } from '@writetome51/array-has';
 import { replaceFirstOf } from './replaceFirstOf_replaceFirstOfEach';
+import { errorIfNotPopulatedArray } from 'error-if-not-populated-array';
 
 
 // value cannot be object.
@@ -14,6 +15,11 @@ export function replaceAllOf(value, newValue, array): void {
 // values cannot contain object.
 
 export function replaceAllOfEach(values: any[], newValues: any[], array) {
+	Object.values(arguments).forEach((arr) => {
+		errorIfNotPopulatedArray(arr);
+	});
+	if (newValues.length < values.length) throw new Error('The array of new values must have at least the' +
+		' same length as the array of values to replace');
 	let i = -1;
 	while (++i < values.length) {
 		replaceAllOf(values[i], newValues[i], array);
--- lib/replaceFirstOf_replaceFirstOfEach.ts
+++ lib/replaceFirstOf_replaceFirstOfEach.ts
@@ -1,7 +1,8 @@
 import { getFirstIndexOf } from '@writetome51/array-get-indexes-basic';
 import { ifIndexNotNegative_getActionResult }
 	from '@writetome51/array-and-index-validation/ifIndexNotNegative_getActionResult';
-import { replaceAt } from '@writetome51/array-replace-at/replaceAt';
+import { replaceAt } from '@writetome51/array-replace-at';
+import { errorIfNotPopulatedArray } from 'error-if-not-populated-array';
 
 
 // Won't work if value is object.
@@ -15,6 +16,11 @@ export function replaceFirstOf(value, newValue, array): void {
 // Won't work if values contains object.
 
 export function replaceFirstOfEach(values: any[], newValues: any[], array): void {
+	Object.values(arguments).forEach((arr) => {
+		errorIfNotPopulatedArray(arr);
+	});
+	if (newValues.length < values.length) throw new Error('The array of new values must have at least the' +
+		' same length as the array of values to replace');
 	let i = -1;
 	while (++i < values.length) {
 		replaceFirstOf(values[i], newValues[i], array);
