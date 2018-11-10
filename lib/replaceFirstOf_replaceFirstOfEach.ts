4cd6cbcd9c3a05d7c5de6fd40940274b9069ad50
Steve Thompson <writetome51@gmail.com>
2018-11-10 03:08:50 -0700

    minor change

--- lib/replaceFirstOf_replaceFirstOfEach.ts
+++ lib/replaceFirstOf_replaceFirstOfEach.ts
@@ -3,8 +3,8 @@ import { getFirstIndexOf } from '@writetome51/array-get-indexes-basic';
 import { ifIndexNotNegative_getActionResult }
 	from '@writetome51/array-and-index-validation/ifIndexNotNegative_getActionResult';
 import { replaceAt } from '@writetome51/array-replace-at';
-import { _doMultiReplace } from './privy/private_functions';
 import { arraysMatch } from '@writetome51/arrays-match';
+import { _doMultiReplace } from './privy/private_functions';
 
 
 // Won't work if value is object.
