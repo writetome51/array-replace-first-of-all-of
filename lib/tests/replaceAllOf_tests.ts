c0fa59d8be89a9c727e4db672b502b6cbdd34c5c
Steve Thompson <writetome51@gmail.com>
2018-11-10 02:59:52 -0700

    finished adding tests

--- lib/tests/replaceAllOf_tests.ts
+++ lib/tests/replaceAllOf_tests.ts
@@ -1,4 +1,4 @@
-import { replaceAllOfEach } from './replaceAllOf_replaceAllOfEach';
+import { replaceAllOf } from '../replaceAllOf_replaceAllOfEach';
 import { arraysMatch } from '@writetome51/arrays-match';
 
 
@@ -7,8 +7,8 @@ let arrCopy = arr;
 
 
 // Test 1: make sure it works for numbers:
-replaceAllOfEach([1, 2, 3], [10, 20, 30], arr);
-if (arraysMatch(arr, [10, 20, 30, 10, 20, 30, 10, 20, 30, '1', '2', '3'])) console.log('test 1 passed');
+replaceAllOf(2, 20, arr);
+if (arraysMatch(arr, [1, 20, 3, 1, 20, 3, 1, 20, 3, '1', '2', '3'])) console.log('test 1 passed');
 else console.log('test 1 FAILED');
 
 
@@ -19,24 +19,23 @@ else console.log('test 1A FAILED');
 
 // Test 2: make sure function performs normally when there's only 1 instance of item to replace:
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-replaceAllOfEach([2], [20], arr);
+replaceAllOf(2, 20, arr);
 if (arraysMatch(arr, [1, 20, 3, 4, 5, 6, 7, 8, 9])) console.log('test 2 passed');
 else console.log('test 2 FAILED');
 
 
 // Test 3: make sure it works for strings:
 arr = ['aa', 'a', 'aa', 'a', 'aa', 'a', 'aa', 'a', 10, 'aa', 1];
-replaceAllOfEach(['a', 'aa'], ['z', 'cc'], arr);
-if (arraysMatch(arr, ['cc', 'z', 'cc', 'z', 'cc', 'z', 'cc', 'z', 10, 'cc', 1]))
-	console.log('test 3 passed');
+replaceAllOf('a', 'z', arr);
+if (arraysMatch(arr, ['aa', 'z', 'aa', 'z', 'aa', 'z', 'aa', 'z', 10, 'aa', 1])) console.log('test 3 passed');
 else console.log('test 3 FAILED');
 
 
 // Test 4: make sure it works for booleans:
 arr = [true, false, true, false, 1, 0, 1, undefined];
 arrCopy = arr;
-replaceAllOfEach([false, true], ['no', 'yes'], arr);
-if (arraysMatch(arr, ['yes', 'no', 'yes', 'no', 1, 0, 1, undefined])) console.log('test 4 passed');
+replaceAllOf(false, 'no', arr);
+if (arraysMatch(arr, [true, 'no', true, 'no', 1, 0, 1, undefined])) console.log('test 4 passed');
 else console.log('test 4 FAILED');
 
 
@@ -48,8 +47,8 @@ else console.log('test 4A FAILED');
 // Test 5: make sure it works for arrays:
 arr = ['bb', ['bb'], ['b'], 'bb', ['bb'], ['b'], 'bb'];
 arrCopy = arr;
-replaceAllOfEach([['bb'], ['b']], [[], ['c']], arr);
-if (arraysMatch(arr, ['bb', [], ['c'], 'bb', [], ['c'], 'bb'])) console.log('test 5 passed');
+replaceAllOf(['bb'], [], arr);
+if (arraysMatch(arr, ['bb', [], ['b'], 'bb', [], ['b'], 'bb'])) console.log('test 5 passed');
 else console.log('test 5 FAILED');
 
 
@@ -60,29 +59,30 @@ else console.log('test 5A FAILED');
 
 // Test 6:
 arr = ['', '0', '', '0', '', '0'];
-replaceAllOfEach(['', '0'], [0, ''], arr);
-if (arraysMatch(arr, [0, '', 0, '', 0, ''])) console.log('test 6 passed');
+replaceAllOf('', 0, arr);
+if (arraysMatch(arr, [0, '0', 0, '0', 0, '0'])) console.log('test 6 passed');
 else console.log('test 6 FAILED');
 
 
 // Test 7:
 arr = ['', 0, '', 0, '', 0];
-replaceAllOfEach(['', 0], [0, 1], arr);
-if (arraysMatch(arr, [1, 1, 1, 1, 1, 1])) console.log('test 7 passed');
+replaceAllOf('', 1, arr);
+if (arraysMatch(arr, [1, 0, 1, 0, 1, 0])) console.log('test 7 passed');
 else console.log('test 7 FAILED');
 
 
 // Test 8: make sure it's ok to replace items with objects:
-arr = ['', 0, '', 0, '', 0];
-replaceAllOfEach(['', ''], [0, 1], arr);
-if (arraysMatch(arr, [0, 0, 0, 0, 0, 0])) console.log('test 8 passed');
+arr = ['bb', ['bb'], ['b'], 'bb', ['bb'], ['b'], 'bb'];
+replaceAllOf(['b'], {prop1: 'b'}, arr);
+if (arr[2].prop1 && arr[5].prop1 && arr[2].prop1 === 'b' && arr[5].prop1 === 'b')
+	console.log('test 8 passed');
 else console.log('test 8 FAILED');
 
 
 // Test 9: should trigger error if first arg is object:
 let errorTriggered = false;
 try {
-	replaceAllOfEach({}, [], arr);
+	replaceAllOf({}, [], arr);
 }
 catch (e) {
 	errorTriggered = true;
@@ -91,23 +91,11 @@ if (errorTriggered) console.log('test 9 passed');
 else console.log('test 9 FAILED');
 
 
-// Test 9A: should trigger error if second arg is object:
-errorTriggered = false;
-try {
-	replaceAllOfEach([1], {}, arr);
-}
-catch (e) {
-	errorTriggered = true;
-}
-if (errorTriggered) console.log('test 9A passed');
-else console.log('test 9A FAILED');
-
-
 // Test 10: should trigger error if oldvalue and newvalue are identical:
 arr = [1, 2, 3, 1, 2, 3, 1, 2, 3];
 errorTriggered = false;
 try {
-	replaceAllOfEach([1, 3], [2, 3], arr);
+	replaceAllOf(1, 1, arr);
 }
 catch (e) {
 	errorTriggered = true;
@@ -120,7 +108,7 @@ else console.log('test 10 FAILED');
 arr = [[1], 2, 3, [1], 2, 3, [1], 2, 3];
 errorTriggered = false;
 try {
-	replaceAllOfEach([[1]], [[1]], arr);
+	replaceAllOf([1], [1], arr);
 }
 catch (e) {
 	errorTriggered = true;
@@ -134,7 +122,7 @@ else console.log('test 10A FAILED');
 arr = [1, 2, 3, 1, 2, 3, 1, 2, 3];
 errorTriggered = false;
 try {
-	replaceAllOfEach([10], [1], arr);
+	replaceAllOf(10, 1, arr);
 }
 catch (e) {
 	errorTriggered = true;
